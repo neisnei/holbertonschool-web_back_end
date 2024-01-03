8af5945535288082a96a26abed9b9815fdac7ccc
neisnei <neischalyruidiaz@gmail.com>
2024-01-03 14:44:16 +0000

    annotations add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''"""type-annotated function add that takes a float
+    Arguments: a: float
+                b: float
+''"""
+
+
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b. '''
+    return a + b
